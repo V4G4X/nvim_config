f569a2b4d021b89f7c13ea4c68deb080843ec582
Varun Gawande <varun.gawande@espressif.com>
2023-07-26 00:10:49 +0530

    Added custom git signs function

--- lua/core/plugin_configs/lualine.lua
+++ lua/core/plugin_configs/lualine.lua
@@ -1,6 +1,39 @@
 -- Initialiez LuaLine
 
 function InitLuaLine()
+	-- Function that returns string for git status
+	local function get_git_status()
+		local status = vim.fn.systemlist("git status --porcelain")
+		local added, unstaged, modified, deleted, untracked = 0, 0, 0, 0, 0
+
+		for _, line in ipairs(status) do
+			local change = line:sub(1, 2)
+			if change == "A " or change == "AM" or change == "M " or change == "MM" then
+				added = added + 1
+			end
+			if change == " M" or change == "MM" then
+				unstaged = unstaged + 1
+				-- modified = modified + 1
+			end
+			if change == " D" or change == "MD" then
+				deleted = deleted + 1
+			end
+			if change == "??" then
+				untracked = untracked + 1
+			end
+		end
+
+		local statusFinal = ""
+		if added > 0 then statusFinal = statusFinal .. string.format("+%d ", added) end
+		if unstaged > 0 then statusFinal = statusFinal .. string.format("!%d ", unstaged) end
+		if modified > 0 then statusFinal = statusFinal .. string.format("!%d ", modified) end
+		if deleted > 0 then statusFinal = statusFinal .. string.format("-%d ", deleted) end
+		if untracked > 0 then statusFinal = statusFinal .. string.format("?%d", untracked) end
+
+		return statusFinal
+		--return string.format("*%d +%d -%d ?%d", added, modified, deleted, untracked)
+	end
+
 	require('lualine').setup {
 		options = {
 			icons_enabled = true,
@@ -22,7 +55,7 @@ function InitLuaLine()
 		},
 		sections = {
 			lualine_a = { 'mode' },
-			lualine_b = { 'branch', 'diff', 'diagnostics' },
+			lualine_b = { 'branch', 'diff', 'diagnostics', get_git_status },
 			lualine_c = { 'filename' },
 			lualine_x = { 'encoding', 'fileformat', 'filetype' },
 			lualine_y = { 'progress' },
