ee7fe9a5b130da00f4a2e75524ab6a91e36dbebb
Varun Gawande <varun.gawande@espressif.com>
2023-07-26 10:42:20 +0530

    Added LSP keybindings

--- lua/core/plugin_configs/lsp.lua
+++ lua/core/plugin_configs/lsp.lua
@@ -1,11 +1,20 @@
 -- Initialise LSP plugin
 
 function InitLspPlugin()
-
     local lsp = require('lsp-zero').preset({})
 
     lsp.on_attach(function(client, bufnr)
         lsp.default_keymaps({ buffer = bufnr })
+        local opts = { buffer = bufnr, remap = false }
+
+        -- Key maps
+        vim.keymap.set("n", "<C-]>", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>l=", function() vim.lsp.buf.format() end, opts)
+        vim.keymap.set("n", "<C-]>", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end)
 
 
