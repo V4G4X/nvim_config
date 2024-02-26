6f7ec3c0485fc6a4cc43235ed8e9319de4e8c55f
Varun Gawande <varun.gawande@espressif.com>
2024-02-26 16:01:16 +0530

    [LuaLine] Brought Filetype back to default location

--- lua/core/plugin_configs/lualine.lua
+++ lua/core/plugin_configs/lualine.lua
@@ -57,9 +57,9 @@ function InitLuaLine()
             lualine_a = { 'mode' },
             lualine_b = { 'branch', 'diff', 'diagnostics', get_git_status },
             lualine_c = { 'filename' },
-            lualine_x = { 'encoding', 'filetype' },
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
             lualine_y = { 'progress' },
-            lualine_z = { 'location', 'fileformat' }
+            lualine_z = { 'location' }
         },
         inactive_sections = {
             lualine_a = {},
