4ca065bada22a5669ff28243e4be5654764029da
Varun Gawande <varun.gawande@espressif.com>
2024-03-11 17:48:22 +0530

    [LSP] gopls updated configurations
    Removed usePlaceholders because it added difficulty in writing function calls.

--- lua/core/plugin_configs/lsp.lua
+++ lua/core/plugin_configs/lsp.lua
@@ -48,10 +48,19 @@ function InitLspPlugin()
             gopls = {
                 gofumpt = true,
                 staticcheck = true,
-                usePlaceholders = true,
                 analyses = {
                     unusedparams = true,
-                }
+                    unusedvariable = true,
+                    unusedwrite = true,
+                },
+                hints = {
+                    assignVariableTypes = true,
+                    compositeLiteralFields = true,
+                    compositeLiteralTypes = true,
+                    constantValues = true,
+                    parameterNames = true,
+                    rangeVariableTypes = true,
+                },
             }
         },
     })
