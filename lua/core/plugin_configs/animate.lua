fe1eb8e91e13ea439c6263e301e2487e53c2ea61
Varun Gawande <varun.gawande@espressif.com>
2024-04-26 10:56:20 +0530

    [Animations] Custom configs

--- lua/core/plugin_configs/animate.lua
+++ lua/core/plugin_configs/animate.lua
@@ -1,7 +1,18 @@
 -- Initialise MiniAnimate plugin
 
 function InitMiniAnimate()
-    require("mini.animate").setup {}
+    local animate = require('mini.animate')
+    animate.setup({
+        cursor = {
+            -- Animate with line-column angle instead of shortest line
+            path = animate.gen_path.angle(),
+            timing = animate.gen_timing.linear({ duration = 25, unit = 'total' }),
+        },
+        resize = {
+            -- Animate for 200 milliseconds with linear easing
+            timing = animate.gen_timing.linear({ duration = 10, unit = 'total' }),
+        },
+    })
 end
 
 if not vim.g.vscode then InitMiniAnimate() end
