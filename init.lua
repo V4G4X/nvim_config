24fc435219ca0f2cee00d2b24912136b0b6e69d3
Varun Gawande <varun.gawande@espressif.com>
2024-04-26 15:58:12 +0530

    [Colors] Some plugins need termgui pre set
    Like nvim-notify

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.opt.termguicolors = true
 require("lazy").setup("plugins")
 
 -- Load plugin configurations
