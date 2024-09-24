6041eed909b40c2775894a6851e3e677db168303
Varun Gawande <varun.gawande@espressif.com>
2024-09-24 21:50:09 +0530

    [Sessions] Ignore on blank buffers

--- lua/plugins/autoSessions.lua
+++ lua/plugins/autoSessions.lua
@@ -1,11 +1,12 @@
 return {
     'rmagatti/auto-session',
-    config = function()
-        require("auto-session").setup {
-            log_level = "error",
-            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-            bypass_session_save_file_types = { 'startup' },
-        }
+    opts = {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+        bypass_session_save_file_types = { 'startup', 'netrw', 'blank' },
+    },
+    config = function(_, opts)
+        require("auto-session").setup(opts)
         vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     end
 }
