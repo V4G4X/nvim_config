de61a258f62280b2206647b72fe52e48ee670393
Varun Gawande <varun.gawande@espressif.com>
2025-02-18 23:16:11 +0530

    [Buffers] Sensible keymaps for cycling through buffers

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -21,11 +21,11 @@ return {
         require("bufferline").setup(opts)
 
         vim.keymap.set("n", "<leader>b", "", { desc = "Buffers" })
-        vim.keymap.set("n", "<leader>bN", "<cmd>BufferLineCyclePrev<cr>", { desc = "Cycle Prev" })
+        vim.keymap.set("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+        vim.keymap.set("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
         vim.keymap.set("n", "<leader>bb", "<cmd>BufferLinePick<cr>", { desc = "Pick Buffer" })
         vim.keymap.set("n", "<leader>bc", "<cmd>BufferLineCloseOthers<cr>", { desc = "Close other Buffers" })
         vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete current buffer" })
-        vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<cr>", { desc = "Cycle Next" })
         vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "Toggle Pin" })
         vim.keymap.set("n", "<leader>bs", "", { desc = "Sort" })
         vim.keymap.set("n", "<leader>bsd", "<cmd>BufferLineSortByDirectory<cr>", { desc = "By Directory" })
