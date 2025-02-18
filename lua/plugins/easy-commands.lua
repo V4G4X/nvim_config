a6b16d5fa03b54d9759cefe238a773d3b13d9cbd
Varun Gawande <varun.gawande@espressif.com>
2025-02-18 23:23:08 +0530

    [QoL] Keymap for closing and clearing session

--- lua/plugins/easy-commands.lua
+++ lua/plugins/easy-commands.lua
@@ -1,17 +1,22 @@
 if not vim.g.vscode then
+    local function ClearSessionAndQuit()
+        vim.cmd("SessionDelete")
+        vim.cmd("QuitNvim")
+    end
+
     return {
         "LintaoAmons/easy-commands.nvim",
         event = "VeryLazy",
+        keys = {
+            { "<leader>Q", ClearSessionAndQuit, desc = "Clear session and quit Neovim", mode = { "n" } },
+        },
         config = function()
             require("easy-commands").setup({
                 ---@type EasyCommand.Command[]
                 myCommands = {
                     {
                         name = "ClearSessionAndQuit",
-                        callback = function()
-                            vim.cmd("SessionDelete")
-                            vim.cmd("QuitNvim")
-                        end,
+                        callback = ClearSessionAndQuit,
                         description = "Clears the current Session before quitting",
                     },
                 }
