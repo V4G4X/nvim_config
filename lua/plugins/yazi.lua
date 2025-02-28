b63b52d3a0ff7d71f0c4aacd805b9b1207a4efb9
Varun Gawande <varun.gawande@espressif.com>
2025-03-01 02:28:05 +0530

    [File] Added Yazi plugin

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,51 @@
+if not vim.g.vscode then
+    ---@type LazySpec
+    return {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        keys = {
+            { "<leader>y", "", desc = "Yazi File Manager" },
+            -- 👇 in this section, choose your own keymappings!
+            {
+                -- Open in the current working directory
+                "<leader>yy",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<leader>yt",
+                mode = { "n", "v" },
+                "<cmd>Yazi toggle<cr>",
+                desc = "Open yazi at the current file",
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            -- customize the keymaps that are active when yazi is open and focused. The
+            -- defaults are listed below. Note that the keymaps simply hijack input and
+            -- they are never sent to yazi, so only try to map keys that are never
+            -- needed by yazi.
+            --
+            -- Also:
+            -- - use e.g. `open_file_in_tab = false` to disable a keymap
+            -- - you can customize only some of the keymaps (not all of them)
+            -- - you can opt out of all keymaps by setting `keymaps = false`
+            keymaps = {
+                show_help = "<f1>",
+                open_file_in_vertical_split = "<m-v>",
+                open_file_in_horizontal_split = "<c-x>",
+                open_file_in_tab = "<c-t>",
+                grep_in_directory = "<c-s>",
+                replace_in_directory = "<c-g>",
+                cycle_open_buffers = "<tab>",
+                copy_relative_path_to_selected_files = "<c-y>",
+                send_to_quickfix_list = "<c-q>",
+                change_working_directory = "<c-\\>",
+            },
+        },
+    }
+end
+
+return {}
