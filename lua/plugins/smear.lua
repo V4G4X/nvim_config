3460cdea7b4ca2adfdc85f0689a959ab28bb9609
Varun Gawande <varun.gawande@espressif.com>
2025-06-12 22:14:42 +0530

    [Animate] Use defaults for Smear
    Plus added keymap that toggles

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -1,13 +1,20 @@
 if not vim.g.vscode then
+	local enabled = true
+	local function toggle()
+		enabled = not enabled
+		require("smear_cursor").enabled = enabled
+		if enabled then
+			print("Smear enabled")
+		else
+			print("Smear disabled")
+		end
+	end
 	return {
 		"sphamba/smear-cursor.nvim",
-		opts = { -- Default  Range
-			time_interval = 7, -- 17 milliseconds
-			stiffness = 0.8, -- 0.6      [0, 1]
-			trailing_stiffness = 0.5, -- 0.4      [0, 1]
-			stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-			trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-			distance_stop_animating = 0.5, -- 0.1      > 0
+		lazy = false,
+		opts = {},
+		keys = {
+			{ "<leader>ts", toggle, desc = "Toggle Smear cursor" },
 		},
 	}
 end
