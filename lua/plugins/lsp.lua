38bf6e0309c1408fc1437ba71bd3c759c195cdee
Varun Gawande <varun.gawande@espressif.com>
2025-08-15 03:15:58 +0530

    [LSP] Add bash language server and shell script formatting support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,6 +57,7 @@ if not vim.g.vscode then
 						yaml = { "yamlfmt" },
 						toml = { "tombi" },
 						markdown = { "prettier" },
+						sh = { "shfmt", "beautysh" },
 					},
 				})
 			end,
@@ -259,10 +260,28 @@ if not vim.g.vscode then
 					root_markers = { "tombi.toml", "pyproject.toml", ".git" },
 				}
 
+				vim.lsp.config["bashls"] = {
+					cmd = { "bash-language-server", "start" },
+					settings = {
+						bashIde = {
+							-- Glob pattern for finding and parsing shell script files in the workspace.
+							-- Used by the background analysis features across files.
+
+							-- Prevent recursive scanning which will cause issues when opening a file
+							-- directly in the home directory (e.g. ~/foo.sh).
+							--
+							-- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
+							globPattern = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command)",
+						},
+					},
+					filetypes = { "bash", "sh" },
+					root_markers = { ".git" },
+				}
+
 				require("lspconfig").yamlls.setup({})
 
 				vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities() })
-				vim.lsp.enable({ "gopls", "lua_ls", "yamlls", "marksman", "pyright", "tombi" })
+				vim.lsp.enable({ "gopls", "lua_ls", "yamlls", "marksman", "pyright", "tombi", "bashls" })
 			end,
 		},
 	}
