9205bcc46e83fca96c30d499e8a5ba66ff730de0
Varun Gawande <varun.gawande@espressif.com>
2025-08-14 22:49:23 +0530

    [Smear] Apply snappier config from repo docs
    Based on optimized settings from:
    https://github.com/sphamba/smear-cursor.nvim

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -12,7 +12,16 @@ if not vim.g.vscode then
 	return {
 		"sphamba/smear-cursor.nvim",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			time_interval = 7, -- milliseconds
+			stiffness = 0.8, -- 0.6      [0, 1]
+			trailing_stiffness = 0.5, -- 0.4      [0, 1]
+			stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+			trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+			damping = 0.8, -- 0.65     [0, 1]
+			damping_insert_mode = 0.8, -- 0.7      [0, 1]
+			distance_stop_animating = 0.5, -- 0.1      > 0
+		},
 		keys = {
 			{ "<leader>ts", toggle, desc = "Toggle Smear cursor" },
 		},
